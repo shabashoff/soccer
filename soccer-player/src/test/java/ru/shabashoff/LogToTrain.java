ddcb232076da6d1d014d9d9b9a55c95253938c4d
shabashoff <niksh81@gmail.com>
2019-04-24 03:13:46 +0300

    Working on collect training data

--- soccer-player/src/test/java/ru/shabashoff/LogToTrain.java
+++ soccer-player/src/test/java/ru/shabashoff/LogToTrain.java
@@ -1,33 +1,275 @@
 package ru.shabashoff;
 
+import lombok.SneakyThrows;
 import org.junit.Test;
-import ru.shabashoff.decision.DecisionTree;
-import ru.shabashoff.entity.Player;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.math.BigDecimal;
+import java.util.*;
 
 public class LogToTrain {
-    TrainPlayer tp = new TrainPlayer(1, "1", null);
 
     @Test
     public void test() throws FileNotFoundException {
-        int side = 0;
-        FileInputStream fi = new FileInputStream("../ts.rcl");
 
+        Set<TrainAction> acts = getTrainSample("../ts.rcl").acts;
+        HashMap<String, List<List<BigDecimal>>> st = new HashMap<>();
+
+        for (TrainAction act : acts) {
+            if (st.containsKey(act.action)) {
+                st.get(act.action).add(act.params);
+            } else {
+                ArrayList<List<BigDecimal>> lst = new ArrayList<>();
+                lst.add(act.params);
+                st.put(act.action, lst);
+            }
+        }
+
+        System.out.println(st);
+    }
+
+
+    @SneakyThrows
+    private TrainSample getTrainSample(String fileName) {
+        HashSet<String> msgs = new HashSet<>(Arrays.asList("kick", "catch", "turn", "move", "dash", "tackle", "turn_neck"));
+
+        FileInputStream fi = new FileInputStream(fileName);
 
         Scanner sc = new Scanner(fi);
 
+        Map<PlayerTime, List<TrainAction>> pars = new HashMap<>();
+        Set<TrainAction> acts = new HashSet<>();
 
-        System.out.println(sc.nextLine());
+        while (sc.hasNext()) {
+            String str = sc.nextLine();
+            String[] line = str.split("[ \t]");
+
+            int l = Integer.parseInt(line[0].substring(0, line[0].indexOf(',')));
+            int r = Integer.parseInt(line[0].substring(line[0].indexOf(',') + 1));
+
+            if (line[1].equals("(referee")) {
+                //TODO
+                continue;
+            }
+
+            String teamName = line[2].substring(0, line[2].indexOf('_'));
+
+            String st = line[2].substring(line[2].indexOf('_') + 1, line[2].indexOf(':'));
+
+            if (st.equals("Coach")) continue;
+
+            int n = Integer.parseInt(st);
+
+            List<String> prs = getMessages(str.substring(line[0].length() + line[1].length() + line[2].length() + 3).replace("(", " (").trim());
+
+            PlayerTime pt = new PlayerTime(n, l + r, 0);
+
+            for (String pr : prs) {
+                List<String> ls = getMessages(pr.substring(1, pr.length() - 1));
+                if (msgs.contains(ls.get(0))) {
+                    List<BigDecimal> bb = new ArrayList<>();
+                    for (int i = 1; i < ls.size(); i++) {
+                        if (!isNumeric(ls.get(i))) continue;
+
+                        bb.add(new BigDecimal(ls.get(i)));
+                    }
+
+                    TrainAction ta = new TrainAction(ls.get(0), bb);
+
+                    if (pars.containsKey(pt)) {
+                        pars.get(pt).add(ta);
+                    } else {
+                        ArrayList<TrainAction> ll = new ArrayList<>();
+                        ll.add(ta);
+                        pars.put(pt, ll);
+                    }
+
+                    acts.add(ta);
+                }
+            }
+        }
+        return new TrainSample(pars, acts);
+    }
+
+    @SneakyThrows
+    private Map<PlayerTime, List<BigDecimal>> getTrainVectors(String fileName) {
+        FileInputStream fi = new FileInputStream(fileName);
+
+        Scanner sc = new Scanner(fi);
+
+        Map<PlayerTime, List<BigDecimal>> pars = new HashMap<>();
+
+        while (sc.hasNext()) {
+            String line = sc.nextLine();
+            String type = parseType(line);
+
+            if (type.equals("show")) {
+                List<String> messages = getMessages(line.substring(line.indexOf(' ') + 1, line.length() - 1));
+
+                long time = Long.parseLong(messages.get(0));
+
+                List<BigDecimal> bls = getBallParameters(messages.get(1));
+
+                for (int i = 2; i < messages.size(); i++) {
+                    List<BigDecimal> p = getParameters(messages.get(i));
+                    p.addAll(bls);
+
+                    pars.put(new PlayerTime((i - 2) % 11, time, i > 10 ? 1 : 0), p);
+                }
+            }
+        }
+
+        return pars;
     }
 
 
-    static class TrainPlayer extends Player {
+    private List<BigDecimal> getParameters(String msg) {
+        List<String> pps = getMessages(msg.substring(1, msg.length() - 1));
+        List<BigDecimal> bls = new ArrayList<>();
 
-        public TrainPlayer(int id, String teamName, DecisionTree tree) {
-            super(id, teamName, tree);
+        bls.add(BigDecimal.valueOf(Double.parseDouble(pps.get(1))));
+        bls.add(new BigDecimal(pps.get(2).charAt(pps.get(2).length() - 1)));
+
+        for (int i = 3; i < 9; i++) {
+            bls.add(BigDecimal.valueOf(Double.parseDouble(pps.get(i))));
+        }
+
+        return bls;
+    }
+
+
+    private List<BigDecimal> getBallParameters(String msg) {
+        List<String> pps = getMessages(msg.substring(1, msg.length() - 1));
+        List<BigDecimal> bls = new ArrayList<>();
+
+        for (int i = 1; i < pps.size(); i++) {
+            bls.add(BigDecimal.valueOf(Double.parseDouble(pps.get(i))));
+        }
+
+        return bls;
+    }
+
+    private static boolean isNumeric(String strNum) {
+        try {
+            double d = Double.parseDouble(strNum);
+        } catch (NumberFormatException | NullPointerException nfe) {
+            return false;
+        }
+        return true;
+    }
+
+    private List<String> getMessages(String message) {
+        List<String> list = new ArrayList<>();
+        char[] chars = message.toCharArray();
+        int start = 0;
+        int count = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '(') count++;
+            else if (chars[i] == ')') count--;
+
+            if ((chars[i] == ' ') && count == 0) {
+                list.add(new String(chars, start, i - start));
+                start = i + 1;
+            }
+        }
+        list.add(new String(chars, start, message.length() - start));
+        return list;
+    }
+
+
+    private String parseType(String message) {
+        if (message.indexOf('(') == -1 || message.indexOf(' ') == -1) return message;
+
+        return message.substring(message.indexOf('(') + 1, message.indexOf(' '));
+    }
+
+
+    static class TrainSample {
+        private final Map<PlayerTime, List<TrainAction>> pars;
+        private final Set<TrainAction> acts;
+
+        TrainSample(Map<PlayerTime, List<TrainAction>> pars, Set<TrainAction> acts) {
+            this.pars = pars;
+            this.acts = acts;
+        }
+
+        @Override
+        public String toString() {
+            return "TrainSample{" +
+                    "pars=" + pars +
+                    ", acts=" + acts +
+                    '}';
         }
     }
+
+    static class PlayerTime {
+        private final long playerId;
+        private final long time;
+        private final long side;
+
+        PlayerTime(long playerId, long time, long side) {
+            this.playerId = playerId;
+            this.time = time;
+            this.side = side;
+        }
+
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            PlayerTime that = (PlayerTime) o;
+            return playerId == that.playerId &&
+                    time == that.time &&
+                    side == that.side;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(playerId, time, side);
+        }
+
+        @Override
+        public String toString() {
+            return "PlayerTime{" +
+                    "playerId=" + playerId +
+                    ", time=" + time +
+                    ", side=" + side +
+                    '}';
+        }
+    }
+
+    static class TrainAction {
+        private final String action;
+        private final List<BigDecimal> params;
+
+        TrainAction(String action, List<BigDecimal> params) {
+            this.action = action;
+            this.params = params;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            TrainAction that = (TrainAction) o;
+            return action.equals(that.action) &&
+                    params.equals(that.params);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(action, params);
+        }
+
+        @Override
+        public String toString() {
+            return "TrainAction{" +
+                    "action='" + action + '\'' +
+                    ", params=" + params +
+                    '}';
+        }
+    }
+
 }
